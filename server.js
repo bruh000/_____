3969e0a6d9d9338a65fe9af23ca33c78d6e4f05b
Your Name <name@email.com>
2022-05-24 09:21:49 +0700

    234

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: 'ENV_FILENAME' });
-}
-
 const express = require('express')
 const app = express()
+
+const port = 3000
 
 const expressLayouts = require('express-ejs-layouts')
 
@@ -16,12 +14,11 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env?.DATABASE_URL ?? `mongodb://localhost:${port}/___`, { useNewUrlParser: true })
 
 const db = mongoose.connection
 db.on('error', err => console.log(err))
 db.once('error', () => console.log('Connected'))
 
 app.use('/', indexRouter)
-
-app.listen(process.env.PORT)
+app.listen(process.env?.PORT ?? port)
