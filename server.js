596c8d71d994e18ad66f41ae95263e56b3dfe806
Your Name <name@email.com>
2022-05-24 15:28:54 +0700

    123

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const express = require('express')
 const app = express()
 
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,5 +28,6 @@ db.on('error', err => console.log(err))
 db.once('open', () => console.log('Connected to the db'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 app.listen(process.env.PORT)
