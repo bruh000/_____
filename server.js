01ebfb63e24322d828f83f954fccf45f80989957
Your Name <name@email.com>
2022-05-24 12:45:02 +0700

    123

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
-
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
