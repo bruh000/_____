a67442c89ff1d5449375bf60f197896e5c238ff2
Your Name <name@email.com>
2022-05-24 09:28:41 +0700

    324

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
 const express = require('express')
 const app = express()
-
-const port = 3000
 
 const expressLayouts = require('express-ejs-layouts')
 
@@ -14,11 +14,15 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env?.DATABASE_URL ?? `mongodb://localhost:${port}/___`, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 const db = mongoose.connection
 db.on('error', err => console.log(err))
 db.once('error', () => console.log('Connected'))
 
 app.use('/', indexRouter)
-app.listen(process.env?.PORT ?? port)
+
+app.listen(process.env.PORT)
